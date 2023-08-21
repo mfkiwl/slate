ec548b77443dcf65f89b79b4fad5ec529c524fc1
G-Ragghianti <ragghianti@icl.utk.edu>
2023-08-21 16:05:09 -0400

    implemented basic spack build

--- .github/workflows/spack.sh
+++ .github/workflows/spack.sh
@@ -1,8 +1,43 @@
-#!/bin/bash -x
+#!/bin/bash -e
 
-stage=$1
+bstage=$1
 device=$2
+
+set +x
+trap 'echo "# $BASH_COMMAND"' DEBUG
+
+source /etc/profile
 
 git submodule update --init
 
+SRC=$(pwd)
+cd ..
+git clone https://github.com/spack/spack || true
+source spack/share/spack/setup-env.sh
+export HOME=$(pwd)
 
+module load gcc@10.4.0
+if [ "${device}" = "gpu_nvidia" ]; then
+  ARCH=$(nvidia-smi --query-gpu=compute_cap --format=csv,noheader | head -1 | sed -e 's/\.//')
+  SPEC="+cuda cuda_arch=$ARCH %gcc"
+elif [ "${device}" = "gpu_nvidia" ]; then
+  TARGET=$(rocminfo | grep Name | grep gfx | head -1 | awk '{print $2}')
+  SPEC="+rocm amdgpu_target=$TARGET %gcc"
+else
+  SPEC="+sycl %oneapi"
+  module load intel-oneapi-compilers
+fi
+
+if [ "${bstage}" = "test" ]; then
+  TEST="--test=root"
+fi
+
+spack compiler find
+spack test remove slate
+#spack dev-build slate@master $SPEC
+spack spec --fresh $TEST slate@master $SPEC
+
+
+if [ "${bstage}" = "smoke" ]; then
+  spack test run slate
+fi
