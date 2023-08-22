71227ddaa5d7b5e3d68544559ce6c09cbdc8cddc
G-Ragghianti <ragghianti@icl.utk.edu>
2023-08-22 11:21:59 -0400

    moving packages.yaml

--- .github/workflows/spack.sh
+++ .github/workflows/spack.sh
@@ -12,7 +12,7 @@ if [ "${bstage}" = "build" ]; then
   git submodule update --init
   rm -rf spack || true
   git clone -b gragghia/slate_sycl https://github.com/G-Ragghianti/spack
-  cp spack_packages.yaml spack/etc/spack/
+  cp .github/workflows/spack_packages.yaml spack/etc/spack/
 fi
 
 source spack/share/spack/setup-env.sh
